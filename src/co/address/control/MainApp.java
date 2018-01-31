9cfaaf38cdc2895eb0fada82c9eed6d984121a58
Guerrero <jguerrero089@gmail.com>
2018-01-30 22:42:25 -0500

    5 Persistencia de datos con XML ->algunas excepciones

--- src/co/address/control/MainApp.java
+++ src/co/address/control/MainApp.java
@@ -5,7 +5,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import org.controlsfx.dialog.Dialogs;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -14,6 +24,7 @@ import javafx.scene.layout.BorderPane;
 import co.address.model.*;
 import co.address.view.PersonEditDialogController;
 import co.address.view.PersonOverviewController;
+import co.address.view.RootLayoutController;
 
 public class MainApp extends Application {
 
@@ -64,7 +75,7 @@ public class MainApp extends Application {
 	}
 
 	/**
-	 * Initializes the root layout.
+	 * Initializes the root layout and tries to load the last opened person file.
 	 */
 	public void initRootLayout() {
 		try {
@@ -76,9 +87,20 @@ public class MainApp extends Application {
 			// Show the scene containing the root layout.
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
+
+			// Give the controller access to the main app.
+			RootLayoutController controller = loader.getController();
+			controller.setMainApp(this);
+
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+
+		// Try to load last opened person file.
+		File file = getPersonFilePath();
+		if (file != null) {
+			loadPersonDataFromFile(file);
 		}
 	}
 
@@ -154,5 +176,96 @@ public class MainApp extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	/**
+	 * Returns the person file preference, i.e. the file that was last opened. The
+	 * preference is read from the OS specific registry. If no such preference can
+	 * be found, null is returned.
+	 * 
+	 * @return
+	 */
+	public File getPersonFilePath() {
+		Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+		String filePath = prefs.get("filePath", null);
+		if (filePath != null) {
+			return new File(filePath);
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Sets the file path of the currently loaded file. The path is persisted in the
+	 * OS specific registry.
+	 * 
+	 * @param file
+	 *            the file or null to remove the path
+	 */
+	public void setPersonFilePath(File file) {
+		Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+		if (file != null) {
+			prefs.put("filePath", file.getPath());
+
+			// Update the stage title.
+			primaryStage.setTitle("AddressApp - " + file.getName());
+		} else {
+			prefs.remove("filePath");
+
+			// Update the stage title.
+			primaryStage.setTitle("AddressApp");
+		}
+	}
+
+	/**
+	 * Loads person data from the specified file. The current person data will be
+	 * replaced.
+	 * 
+	 * @param file
+	 */
+	public void loadPersonDataFromFile(File file) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(PersonListWrapper.class);
+			Unmarshaller um = context.createUnmarshaller();
+
+			// Reading XML from the file and unmarshalling.
+			PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+
+			personData.clear();
+			personData.addAll(wrapper.getPersons());
+
+			// Save the file path to the registry.
+			setPersonFilePath(file);
+
+		} catch (Exception e) { // catches ANY exception
+			Dialogs.create().title("Error").masthead("Could not load data from file:\n" + file.getPath())
+					.showException(e);
+		}
+	}
+
+	/**
+	 * Saves the current person data to the specified file.
+	 * 
+	 * @param file
+	 */
+	public void savePersonDataToFile(File file) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(PersonListWrapper.class);
+			Marshaller m = context.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			// Wrapping our person data.
+			PersonListWrapper wrapper = new PersonListWrapper();
+			wrapper.setPersons(personData);
+
+			// Marshalling and saving XML to the file.
+			m.marshal(wrapper, file);
+
+			// Save the file path to the registry.
+			setPersonFilePath(file);
+		} catch (Exception e) { // catches ANY exception
+			Dialogs.create().title("Error").masthead("Could not save data to file:\n" + file.getPath())
+					.showException(e);
+		}
 	}
 }
--- src/co/address/model/Person.java
+++ src/co/address/model/Person.java
@@ -2,6 +2,9 @@ package co.address.model;
 
 import java.time.LocalDate;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import co.address.util.LocalDateAdapter;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -102,6 +105,7 @@ public class Person {
 		return city;
 	}
 
+	@XmlJavaTypeAdapter(LocalDateAdapter.class)
 	public LocalDate getBirthday() {
 		return birthday.get();
 	}
